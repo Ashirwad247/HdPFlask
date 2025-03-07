798bb7917f7a58099d8d63a72f863d5357a3b878
johnson <ashirwadayirumala@gmail.com>
2025-03-07 19:36:53 +0530

    Second

--- app.py
+++ app.py
@@ -54,37 +54,13 @@ def signup():
     email = request.args.get('email','')
     number = request.args.get('mobile','')
     password = request.args.get('password','')
-    otp = random.randint(1000,5000)
-    print(otp)
-    msg = EmailMessage()
-    msg.set_content("Your OTP is : "+str(otp))
-    msg['Subject'] = 'OTP'
-    msg['From'] = "evotingotp4@gmail.com"
-    msg['To'] = email
-    
-    
-    s = smtplib.SMTP('smtp.gmail.com', 587)
-    s.starttls()
-    s.login("evotingotp4@gmail.com", "xowpojqyiygprhgr")
-    s.send_message(msg)
-    s.quit()
-    return render_template("val.html")
-
-@app.route('/predict_lo', methods=['POST'])
-def predict_lo():
-    global otp, username, name, email, number, password
-    if request.method == 'POST':
-        message = request.form['message']
-        print(message)
-        if int(message) == otp:
-            print("TRUE")
-            con = sqlite3.connect('signup.db')
-            cur = con.cursor()
-            cur.execute("insert into `info` (`user`,`email`, `password`,`mobile`,`name`) VALUES (?, ?, ?, ?, ?)",(username,email,password,number,name))
-            con.commit()
-            con.close()
-            return render_template("signup-in.html")
+    con = sqlite3.connect('signup.db')
+    cur = con.cursor()
+    cur.execute("insert into `info` (`user`,`email`, `password`,`mobile`,`name`) VALUES (?, ?, ?, ?, ?)",(username,email,password,number,name))
+    con.commit()
+    con.close()
     return render_template("signup-in.html")
+
 
 @app.route("/signin")
 def signin():
